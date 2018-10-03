cf89c3b1a42645051e2eef0e8d81035602a01d58
hankdu <r23du@uwaterloo.ca>
2018-10-03 11:29:28 -0400

    fixed bug

--- a1ece650.py
+++ a1ece650.py
@@ -3,7 +3,7 @@ import sys
 import re
 
 class CaseInsensitiveDict(dict):
-    @classmethod
+	@classmethod
 	def _k(cls, key):
 		return key.lower() if isinstance(key, basestring) else key
 
@@ -15,7 +15,7 @@ class CaseInsensitiveDict(dict):
 	def __setitem__(self, key, value):
 		super(CaseInsensitiveDict, self).__setitem__(self.__class__._k(key), value)
 	def __delitem__(self, key):
-		return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key)
+		return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key))
 	def __contains__(self, key):
 		return super(CaseInsensitiveDict, self).__contains__(self.__class__._k(key))
 	def has_key(self, key):
@@ -34,55 +34,59 @@ class CaseInsensitiveDict(dict):
 			v = super(CaseInsensitiveDict, self).pop(k)
 			self.__setitem__(k, v)
 
-def initialization():
-	#menu = {"a","c","r","g"}
-	re_vertex = re.compile('\(-?[0-9]+,-?[0-9]+\)')
-	re_street = re.compile('\"[ a-z]+\"')
-	db = CaseInsensitiveDict()
+class CameraData(object):
+	def __init__(self,db={}):
+		self.re_vertex = re.compile('\(-?[0-9]+,-?[0-9]+\)')
+		self.re_street = re.compile('\"[a-z ]+\"')
+		self.db = db
 	
-	return menu
-
-def add(self, arg_list[]):
-	if re_street.match(arg_list[0]):
-		street = cmd
-			
-	for arg in arg_list[1:]:
-		if re_vertex.match(arg):
-			#TODO Distinguish no space between 
-			vertex_list.extend(re_vertex.finditer(arg))
+	def add(self, arg_list):
+		street = self.re_street.match(arg_list)
+		print(street)
+		if street != None:
+			vertex_list = []
 		else:
-			errprt("Invalid 'add' Command","The format of argument {0} is invalid".format(cmd))
+			errprt("Invalid 'add' Command","The format of argument {0} is invalid".format(arg_list))
 			return
-			
-	db[street] = vertex_list
+		
+		if self.re_vertex.match(arg_list):
+			vertex_list.extend(self.re_vertex.finditer(arg_list))
+		else:
+			errprt("Invalid 'add' Command","The format of argument {0} is invalid".format(arg_list))
+			return
+				
+		self.db[street] = vertex_list
 
-def change(self, arg_list[]):
-	if re_street.match(arg_list[0]):
-		if db.has_key(arg_list[0])
-			for arg in arg_list[1:]:
-				if re_vertex.match(arg):
-					#TODO Distinguish no space between 
-					vertex_list.extend(re_vertex.finditer(arg))
+	def change(self, arg_list):
+		street = self.re_street.match(arg_list)
+		if street != None:
+			if self.db.has_key(street):
+				vertex_list = []
+				if self.re_vertex.match(arg):
+					vertex_list.extend(self.re_vertex.finditer(arg))
 				else:
-					errprt("Invalid 'add' Command","The format of argument {0} is invalid".format(cmd))
-				db[street] = vertex_list
+					errprt("Invalid 'add' Command","The format of argument {0} is invalid".format(arg))
+					db[street] = vertex_list
+			else:
+				errprt("Street Not Found","Street {0} does NOT exist in the system or it has already been removed".format(street))
 		else:
-			errprt("Street Not Found","Street {0} does NOT exist in the system or it has already been removed".format(street))
-	else:
-		errprt("Invalid Argument", "Format for street argument is invalid")
+			errprt("Invalid Argument", "Format for street argument is invalid")
 
 
-def remove(self, street):
-	if re_street.match(street):
-		if db.has_key(street)
-			del db[street]
+	def remove(self, street):
+		if self.re_street.match(street):
+			if self.db.has_key(street):
+				del self.db[street]
+			else:
+				errprt("Street Not Found","Street {0} does NOT exist in the system or it has already been removed".format(street))
 		else:
-			errprt("Street Not Found","Street {0} does NOT exist in the system or it has already been removed".format(street))
-	else:
-		errprt("Invalid Argument", "Format for street argument is invalid")
+			errprt("Invalid Argument", "Format for street argument is invalid")
 
-def graph(self):
+	def graph(self):
+		print("???")
 
+	
+#Global Function
 def errprt(msg,reason):
 	print("Error: {0}. Possible Reason: {1}".format(msg,reason))
 	
@@ -90,40 +94,41 @@ def helpprt():
 	#TODO
 	print("HELP ME PLEASE")
 
-def main_loop(init):
-	menu = {"a","c","r","g"}
+def initialization():
+	#menu = {"a","c","r","g"}
+	db = CaseInsensitiveDict({})
+	data = CameraData(db)
+	return data
+
+def main_loop(data):
 	is_quit = False
 	while not is_quit:
-		commands = raw_input('>> ').strip().split()
-		for cmd in commands:
-			cmd_list = list(cmd)
 		
-		if(cmd_list[0]=='a'):
-			add(cmd_list[1:])
-		elif(cmd_list[0]=='c'):
-			change(cmd_list[1:])
-		elif(cmd_list[0]=='r' && len(cmd_list)==2):
-			remove(cmd_list[1])
-		elif(cmd_list[0]=='g' && len(cmd_list)==1):
-			graph()
+		command = raw_input('>> ')
+		cmd_list = command.split(' ')
+		#for cmd in commands:
+		#	cmd_list = list.append(cmd)
+		
+		print(cmd_list)
+		if((cmd_list[0]=='a') & (len(cmd_list)>2)):
+			data.add(command)
+		elif((cmd_list[0]=='c') & (len(cmd_list)>2)):
+			data.change(command)
+		elif((cmd_list[0]=='r') & (len(cmd_list)==2)):
+			data.remove(command)
+		elif((cmd_list[0]=='g') & (len(cmd_list)==1)):
+			data.graph()
 		elif(cmd_list[0]=='q'):
 			is_quit = True
 		else:
-			errprt("Command [{0}] is not valid".format(cmd_list[0]),"Wrong command name or incorrect arguments")
-		
-		#original code from Prof
-		line = sys.stdin.readline()
-		if line == '':
-		break
-		print 'read a line:', line
-
-    # return exit code 0 on successful termination
-    sys.exit(0)
+			errprt("Command [{0}] is not valid".format(cmd_list),"Wrong command name or incorrect arguments")
+	# return exit code 0 on successful termination
+	sys.exit(0)
 
 if __name__ == '__main__':
-	init = initialization()
+	data = initialization()
 	try:	
-		main_loop(init)
+		main_loop(data)
 	except KeyboardInterrupt:
 		clear
 		sys.exit(0)
